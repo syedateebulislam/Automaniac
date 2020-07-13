3b1dcc785f30909af3d4592a54b357d3c9c1c8e9
ateeb <syedateebulislam@gmail.com>
2020-07-13 19:42:26 +0530

    upload file 2.0

--- src/com/MainFiles/FileUpload.java
+++ src/com/MainFiles/FileUpload.java
@@ -1,7 +1,11 @@
 package com.MainFiles;
 
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -10,9 +14,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
 import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 
 /*
@@ -27,30 +35,21 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 @MultipartConfig
 public class FileUpload extends HttpServlet {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{		
-		System.out.println("entered into servlet.");
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Entered into upload servlet...");
 		String ip1 = request.getParameter("input1");
-		String ip2 = request.getParameter("input2");
 		String sv1 = request.getParameter("sampleValue1");
+		String ip2 = request.getParameter("input2");
 		String sv2 = request.getParameter("sampleValue2");
-		System.out.println(ip1+sv1+ip2+sv2);
+		String abtScr = request.getParameter("aboutScript");
+		System.out.println(ip1+","+sv1+","+ip2+","+sv2+","+abtScr);
 		
-		ServletFileUpload sf = new ServletFileUpload(new DiskFileItemFactory());
-		try {
-			List<FileItem> uploadedFiles = sf.parseRequest(request);
-			
-			for(FileItem item: uploadedFiles) {
-				item.write(new File("/Users/Dell/Desktop/Office Stuff/Automaniac/src/com/UploadedFiles/"+item.getName()));
-			}			
-			System.out.println("File Uploaded Successfully...");//add js here for success notification
-		} catch (FileUploadException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		response.sendRedirect("editpage_html.html");
+		Part filePart = request.getPart("file");
+	    String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+	    InputStream fileContent = filePart.getInputStream();
+	    File targetFile = new File("/Users/Dell/Desktop/Office Stuff/Automaniac/src/com/UploadedFiles/"+fileName);
+	    FileUtils.copyInputStreamToFile(fileContent, targetFile);
+	    
+	    response.sendRedirect("editpage_html.html");
 	}
 }
